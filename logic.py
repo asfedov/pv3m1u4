68b5ab379293edf0c76cecad1d3b13ecdb6cafbc
ahiehe <ahiehe@outlook.com>
2025-10-05 16:13:22 +0200

    added level system, shiny, and check pokemon decorator

--- logic.py
+++ logic.py
@@ -3,17 +3,52 @@ import requests
 
 class Pokemon:
     pokemons = {}
+
+    level_up_xp = [0, 10, 33, 80, 156, 270, 428, 640, 911, 1250, 1663, 2160, 2746, 3430, 4225, 5139, 6192, 7390, 8749, 10285]
+
+    scale_factor = 1.4
+    shiny_scale_factor = 1.5
+
     # Инициализация объекта (конструктор)
     def __init__(self, pokemon_trainer, pokemon_number=None):
 
         self.pokemon_trainer = pokemon_trainer   
+
+        self.is_shiny = self.check_shiny()
 
         self.pokemon_number = randint(1,1000) if not pokemon_number or pokemon_number < 0 or pokemon_number > 1000 else pokemon_number
         self.img = self.get_img()
         self.name = self.get_name()
         self.stats = self.get_stats()
 
+        self.level = 0
+        self.xp = 0
+
         Pokemon.pokemons[pokemon_trainer] = self
+
+    def check_shiny(self):
+        return True if randint(1,10) <= 4 else False
+
+    def feed(self):
+        self.xp += 5 + randint(1,5)
+        if self.xp >= Pokemon.level_up_xp[self.level+1]:
+            self.level_up()
+            return True
+        return False
+    
+    def level_info(self):
+        return f"Уровень: {self.level}, XP: {self.xp}/{Pokemon.level_up_xp[self.level + 1]}"
+    
+    def level_up(self):
+        self.xp -= Pokemon.level_up_xp[self.level+1]
+        self.level += 1
+
+        for stat in self.stats.keys():
+            self.scale_stat(stat, Pokemon.shiny_scale_factor if self.is_shiny else Pokemon.scale_factor)
+
+    def scale_stat(self, stat_name, factor):
+        self.stats[stat_name] = int(self.stats[stat_name] * factor)
+
 
     # Метод для получения картинки покемона через API
     def get_img(self):
@@ -21,7 +56,7 @@ class Pokemon:
         response = requests.get(url)
         if response.status_code == 200:
             data = response.json()
-            return (data["sprites"]["other"]["official-artwork"]["front_default"])
+            return (data["sprites"]["other"]["official-artwork"]["front_default" if not self.is_shiny else "front_shiny"])
         else:
             return "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
     
@@ -32,7 +67,7 @@ class Pokemon:
             data = response.json()
             stats = {}
             for stat in data['stats']:
-                stats[stat['stat']['name']] = stat['base_stat']
+                stats[stat['stat']['name']] = stat['base_stat'] * ( Pokemon.shiny_scale_factor if self.is_shiny else 1)
             return stats
         else:
             return {}
@@ -51,18 +86,32 @@ class Pokemon:
         response = requests.get(url)
         if response.status_code == 200:
             data = response.json()
-            return (data['forms'][0]['name'])
+            return (("Shiny " if self.is_shiny else "") + data['forms'][0]['name'])
         else:
             return "Pikachu"
 
 
     # Метод класса для получения информации
     def info(self):
-        return f"Имя твоего покеомона: {self.name}"
+        return f"""Имя твоего покемона: {self.name}
+    Статы покемона: {self.stats}
+    {self.level_info()}"""
 
     # Метод класса для получения картинки покемона
     def show_img(self):
         return self.img
 
+
+
+def check_pokemon(bot):
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            if args[0].from_user.id not in Pokemon.pokemons.keys():
+                bot.reply_to(args[0], "Ты ещё не создал себе покемона, введи /go")
+                return
+            
+            func(*args, **kwargs)
+        return wrapper
+    return decorator
 
 
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import telebot 
 from config import token
 
-from logic import Pokemon
+from logic import Pokemon, check_pokemon
 
 bot = telebot.TeleBot(token) 
 
@@ -11,8 +11,8 @@ def start(message):
 
 @bot.message_handler(commands=['go'])
 def go(message):
-    if message.from_user.username not in Pokemon.pokemons.keys():
-        pokemon = Pokemon(message.from_user.username)
+    if message.from_user.id not in Pokemon.pokemons.keys():
+        pokemon = Pokemon(message.from_user.id)
         bot.send_message(message.chat.id, pokemon.info())
         bot.send_photo(message.chat.id, pokemon.show_img())
     else:
@@ -21,30 +21,34 @@ def go(message):
 @bot.message_handler(commands=['cheat'])
 def cheat(message):
     arguments = telebot.util.extract_arguments(message.text).split()
-    pokemon = Pokemon(message.from_user.username, int(arguments[0]))
+    pokemon = Pokemon(message.from_user.id, int(arguments[0]))
     bot.send_message(message.chat.id, pokemon.info())
     bot.send_photo(message.chat.id, pokemon.show_img())
 
 
 @bot.message_handler(commands=['show'])
+@check_pokemon(bot)
 def show(message):
-    if message.from_user.username in Pokemon.pokemons.keys():
-        pokemon = Pokemon.pokemons[message.from_user.username]
-        bot.send_message(message.chat.id, pokemon.info())
-        bot.send_photo(message.chat.id, pokemon.show_img())
-        bot.send_message(message.chat.id, f"Статы покемона: {pokemon.stats}")
-    else:
-        bot.reply_to(message, "Ты ещё не создал себе покемона, введи /go")
+    pokemon = Pokemon.pokemons[message.from_user.id]
+    bot.send_photo(message.chat.id, pokemon.show_img())
+    bot.send_message(message.chat.id, pokemon.info())
+
 
 @bot.message_handler(commands=['setstat'])
+@check_pokemon(bot)
 def set_stat(message):
-    if message.from_user.username in Pokemon.pokemons.keys():
-        arguments = telebot.util.extract_arguments(message.text).split()
-        pokemon = Pokemon.pokemons[message.from_user.username]
-        is_changed = pokemon.set_stat(arguments[0], int(arguments[1]))
-        bot.send_message(message.chat.id, f"Стат {arguments[0]} изменён на {arguments[1]}" if is_changed else f"Стат {arguments[0]} не найден у покемона {pokemon.name}")
-    else:
-        bot.reply_to(message, "Ты ещё не создал себе покемона, введи /go")
+    arguments = telebot.util.extract_arguments(message.text).split()
+    pokemon = Pokemon.pokemons[message.from_user.id]
+    is_changed = pokemon.set_stat(arguments[0], int(arguments[1]))
+    bot.send_message(message.chat.id, f"Стат {arguments[0]} изменён на {arguments[1]}" if is_changed else f"Стат {arguments[0]} не найден у покемона {pokemon.name}")
+
+
+@bot.message_handler(commands=['feed'])
+@check_pokemon(bot)
+def feed(message):
+    pokemon = Pokemon.pokemons[message.from_user.id]
+    is_levelup = pokemon.feed()
+    bot.send_message(message.chat.id, f"Ты покормил своего покемона {pokemon.name}. {"Он достиг " + str(pokemon.level) + "уровня! Поздравляю!" if is_levelup else "" }\n{pokemon.level_info()}")
 
 
 bot.infinity_polling(none_stop=True)
